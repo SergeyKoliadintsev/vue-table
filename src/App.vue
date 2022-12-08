275c3acadd7079f2f5d6e15e49f4ca68b744be99
Yegor Korenkov <eg.korenkov00@gmail.com>
2022-12-08 11:25:12 +0200

    add infinity scroll

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,71 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import useTable from "@/hooks/useTable";
 
 const selectColumn = ref(["Name", "Age", "Created On", "Percent", "Actions"]);
+
+const testRows = [
+  {
+    id: 1,
+    name: "John",
+    age: 20,
+    createdAt: "2011-10-31",
+    score: 0.03343,
+    test1: "gdsag1",
+    test2: "fsadgasdg",
+  },
+  {
+    id: 2,
+    name: "Jane",
+    age: 24,
+    createdAt: "2011-10-31",
+    score: 0.03343,
+    test1: "gdsag1",
+    test2: "fsadgasdg",
+  },
+  {
+    id: 3,
+    name: "Susan",
+    age: 16,
+    createdAt: "2011-10-30",
+    score: 0.03343,
+    test1: "gdsag1",
+    test2: "fsadgasdg",
+  },
+  {
+    id: 4,
+    name: "Chris",
+    age: 55,
+    createdAt: "2011-10-11",
+    score: 0.03343,
+    test1: "gdsag1",
+    test2: "fsadgasdg",
+  },
+  {
+    id: 5,
+    name: "Dan",
+    age: 40,
+    createdAt: "2011-10-21",
+    score: 234343.0334343,
+    test1: "gdsag1",
+    test2: "fsadgasdg",
+  },
+  {
+    id: 6,
+    name: "John",
+    age: 20,
+    createdAt: "2011-10-31",
+    score: 0.03343,
+    test1: "gdsag1",
+    test2: "fsadgasdg",
+  },
+];
 
 const columns = [
   {
     label: "Name",
     field: "name",
     sortable: false,
-    // width: "20%",
   },
   {
     label: "Age",
@@ -17,42 +73,23 @@ const columns = [
     type: "number",
     sortable: false,
     tdClass: "text-align-center",
-    // width: "10%",
   },
-  // {
-  //   label: "Created On",
-  //   field: "createdAt",
-  //   type: "date",
-  //   dateInputFormat: "yyyy-MM-dd",
-  //   dateOutputFormat: "MMM do yy",
-  //   sortable: false,
-  //   tdClass: "text-align-center",
-  //   thClass: "text-align-center",
-  //   // width: "25%",
-  // },
+  {
+    label: "Created On",
+    field: "createdAt",
+    type: "date",
+    dateInputFormat: "yyyy-MM-dd",
+    dateOutputFormat: "MMM do yy",
+    sortable: false,
+    tdClass: "text-align-center",
+    thClass: "text-align-center",
+  },
   {
     label: "Percent",
     field: "score",
     type: "percentage",
     sortable: false,
-    // width: "10%",
   },
-  // {
-  //   label: "TEST1",
-  //   field: "test1",
-  //   sortable: false,
-  // },
-  // {
-  //   label: "TEST2",
-  //   field: "test2",
-  //   sortable: false,
-  // },
-  // {
-  //   label: "Actions",
-  //   field: "actions",
-  //   sortable: false,
-  //   thClass: "text-align-right",
-  // },
 ];
 const rows = ref([
   {
@@ -129,23 +166,22 @@ const checkSelectedRows = () => {
 };
 
 const clicks = (data: any) => console.log(data);
+
+onMounted(() => {
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+  const callback = () => rows.value.push(...testRows);
+  const observer = new IntersectionObserver(callback, options);
+  const observeredEl = document.querySelector(".observer");
+  observer.observe(observeredEl);
+});
 </script>
 
 <template>
   <div>
-    <!-- <button @click="checkSelectedRows">Check Selected Rows</button>
-    <div>
-      <div v-for="column in selectColumn" :key="column">
-        <input
-          type="checkbox"
-          :id="column"
-          :value="column"
-          v-model="selectedColumns"
-        />
-        <label :for="column">{{ column }}</label>
-      </div>
-    </div> -->
-
     <vue-good-table
       ref="table_fef"
       :columns="columns"
@@ -177,7 +213,7 @@ const clicks = (data: any) => console.log(data);
         </span>
       </template>
       <template v-slot:table-actions-bottom>
-        <button @click="loadMore">Load more</button>
+        <button class="observer" @click="loadMore">Load more</button>
       </template>
     </vue-good-table>
   </div>
